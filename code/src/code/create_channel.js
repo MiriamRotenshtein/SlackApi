2c1601311b7d053a0d791fe0f0280067ef175662
Miriam Rotenshtein <MiriamR@skyvar.co.il>
2023-07-04 10:51:27 +0300

    add

--- code/src/code/create_channel.js
+++ code/src/code/create_channel.js
@@ -1,5 +1,5 @@
 const { WebClient } = require('@slack/web-api');
-const token = 'xoxp-5460143591990-5466679292707-5466955470851-7d771370546f29ed6354686898c6c3d9'
+const token = 'xoxp-5460143591990-5466679292707-5524594054370-f7f0bdcb0ef8f6db5eab49f8369b7bfd'
 const web = new WebClient(token);
 
 class createChannel{
