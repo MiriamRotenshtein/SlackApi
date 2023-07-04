d2bc6f1316c7c734f787b1c0d325fac94b325de3
Miriam Rotenshtein <MiriamR@skyvar.co.il>
2023-07-04 10:03:38 +0300

    add

--- code/main.js
+++ code/main.js
@@ -1,12 +1,6 @@
 const createChannel=require('./src/code/create_channel')
-// const findConversation = require('./src/code/channelsList')
-// const addUser=require('./src/code/addUser')
 
 let create_channel=new createChannel()
-// let add_user=new addUser()
 
 create_channel.create_channel("change")
-// add_user.add_user("U05DMS5RWMB")
-// console.log(create_channel.findConversation("task"))
-// console.log(findConversation("hello"));
 
